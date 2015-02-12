016b8aa59b5d683d7ce0e68c34fccea29d722e91
iko21 <bad_boylp2112@yahoo.com>
2015-02-12 17:01:38 -0600

    nueva pestaña Register

---
+++ StackOverFlow.Domain/Entities/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackOverFlow.Domain.Entities
+{
+    class Account : IEntity
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Account()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ StackoverFlow.Web/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackoverFlow.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View(new AccountRegisterModel ());
+        }
+    }
+
+    public class AccountRegisterModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
